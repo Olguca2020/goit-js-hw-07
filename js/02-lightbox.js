6154282ca59e3f88667fd3c791d8128c20e335e8
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-05-12 09:49:31 +0300

    th2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const cardsContainer = document.querySelector(`.gallery`);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+cardsContainer.insertAdjacentHTML(`beforeend`, galleryMarkup);
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+    })
+    .join("");
+}
+
+cardsContainer.addEventListener(`click`, openLargeCard);
+function openLargeCard(e) {
+  e.preventDefault();
+  if (e.target.tagName !== `IMG`) {
+    return;
+  } else {
+    let gallery = new SimpleLightbox(".gallery a", {
+      captions: true,
+      captionSelector: "img",
+      captionType: "attr",
+      captionsData: "alt",
+      captionPosition: "bottom",
+      captionDelay: 250,
+    });
+    gallery.on("show.simplelightbox", function () {});
+  }
+}
